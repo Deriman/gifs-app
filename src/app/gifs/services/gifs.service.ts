843b04a9c39a7600811e881f271fd9e686aee63a
Jose C. Santos <zderimanz@hotmail.com>
2024-11-03 11:12:28 +0100

    Creamos la interface para tipar la repuesta del servicio a traves de quicktype.io http.get es un generico y utilizamos la interface para tipar la lista de gifs que nos devuelve el servicio.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
@@ -7,6 +8,7 @@ export class GifsService {
     private _tagHistory: string[] = [];
     private apiGifsKey: string = 'KLC7l6VYyShffuFhkZI8IkhLxr5cDsHu';
     private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
+    public gifsList: Gif[] = [];
 
 
     constructor( private http: HttpClient ) { }
@@ -35,7 +37,11 @@ export class GifsService {
             .set('limit', '10')
             .set('q', tag)
 
-        this.http.get(`${this.serviceUrl}/search`, { params })
-            .subscribe( resp => console.log(resp));
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+            .subscribe( 
+                resp => {
+                    this.gifsList = resp.data;
+                    console.log(this.gifsList);
+                });
     }
 }
