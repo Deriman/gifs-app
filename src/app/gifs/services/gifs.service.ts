0d9f4b3b0eb77edabc0c40c608d919bb2af6c2d7
Jose C. Santos <zderimanz@hotmail.com>
2024-11-03 09:53:13 +0100

    Limitar el historial a 10. Validaciones de no tener repetidos ni campos vacios en el GifsService

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,8 +10,19 @@ export class GifsService {
         return [...this._tagHistory];
     }
 
+    private organizeHistory( tag: string){
+        tag = tag.toLowerCase();
+
+        if (this._tagHistory.includes(tag)) {
+            this._tagHistory = this._tagHistory.filter((oldTag) => oldTag != tag )
+        }
+
+        this._tagHistory.unshift(tag);
+        this._tagHistory = this._tagHistory.slice(0,10);
+    }
+
     searchTag( tag: string):void {
         if (tag === "") return
-        this._tagHistory.unshift(tag);
+        this.organizeHistory(tag);
     }
 }
