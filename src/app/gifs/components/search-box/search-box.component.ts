46d2a046e1f968b88ecfddb1aabc6db9f6f64785
Jose C. Santos <zderimanz@hotmail.com>
2024-11-03 09:11:24 +0100

    Configuramos el comportamiento de ViewChild en el input

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-box',
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
     <h5>Buscar:</h5>
     <input type="text"
     class="form-control"
-    placeholder="Buscar gifs...">
+    placeholder="Buscar gifs..."
+    (keyup.enter)="searchTag()" 
+     #txtTagInput> <!-- identifico el input con un tag y obtengo su valor en el template-->
     `
 })
 export class SearchBoxComponent {
+
+    @ViewChild("txtTagInput")
+    public inputTag!: ElementRef<HTMLInputElement>;
+
+    searchTag():void {
+        const newTag = this.inputTag.nativeElement.value
+        console.log({ newTag })
+    }
    
 }
