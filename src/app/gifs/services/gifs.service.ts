a775ac8a072acda202bc0f312b77ca70367e0879
Jose C. Santos <zderimanz@hotmail.com>
2024-11-03 10:52:35 +0100

    Configuramos el servicio web con la api_key. Añadimos el provider de Http Lo inyectamos al servicio Configuramos la petición con los parametros

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
     private _tagHistory: string[] = [];
-    constructor() { }
+    private apiGifsKey: string = 'KLC7l6VYyShffuFhkZI8IkhLxr5cDsHu';
+    private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
+
+
+    constructor( private http: HttpClient ) { }
     
     get tagHistory(): string[] {
         return [...this._tagHistory];
@@ -24,5 +29,13 @@ export class GifsService {
     searchTag( tag: string):void {
         if (tag === "") return
         this.organizeHistory(tag);
+
+        const params = new HttpParams()
+            .set('api_key', this.apiGifsKey)
+            .set('limit', '10')
+            .set('q', tag)
+
+        this.http.get(`${this.serviceUrl}/search`, { params })
+            .subscribe( resp => console.log(resp));
     }
 }
